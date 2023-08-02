f1a62a329636179a3a2b41a5233c0e731634f09b
SanjeevThalod <thalodsanjeev@gmail.com>
2023-08-02 19:27:51 +0530

    update

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import DeleteIcon from '@material-ui/icons/Delete'
 import { useCart, useDispatchCart } from '../components/ContextReducer';
 export default function Cart() {
   let data = useCart();
@@ -58,7 +57,7 @@ export default function Cart() {
                 <td>{food.qty}</td>
                 <td>{food.size}</td>
                 <td>{food.price}</td>
-                <td ><button type="button" className="btn p-0"><DeleteIcon style={{color:"white"}} onClick={() => { dispatch({ type: "REMOVE", index: index }) }} /></button> </td></tr>
+                <td ><button type="button" className="btn p-0"><i className='fa-solid fa-trash' style={{color:"white"}} onClick={() => { dispatch({ type: "REMOVE", index: index }) }} /></button> </td></tr>
             ))}
           </tbody>
         </table>
